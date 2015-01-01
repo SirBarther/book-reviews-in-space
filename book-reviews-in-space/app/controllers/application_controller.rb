03c83e7568518de86a9043694bcc491d83046463
Michael Foster <michael.foster@outlook.com>
2015-01-01 18:09:19 -0500

    Adds CarrierWave functionality

--- book-reviews-in-space/app/controllers/application_controller.rb
+++ book-reviews-in-space/app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  #Added for CarrierWave Integration
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  ##
+
   protect_from_forgery with: :exception
+
+  #Added for CarrierWave Integration
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
+      :password_confirmation, :remember_me, :avatar, :avatar_cache) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
+      :password_confirmation, :current_password, :avatar, :avatar_cache) }
+  end
+  ##
+
 end
